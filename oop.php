c280a9472d807653975949206c79cfef9d1828c8
waes2025 <waes_ahmed@yahoo.com>
2025-07-29 20:26:14 +0600

    Assignment template upload

---
+++ oop.php
@@ -0,0 +1,48 @@
+<?php
+
+class Book {
+
+ // TODO: Add properties as Private
+
+ 
+ public function __construct($title, $availableCopies) {
+
+     // TODO: Initialize properties
+
+     }
+
+// TODO: Add getTitle method
+
+ // TODO: Add getAvailableCopies method
+
+ 
+ // TODO: Add borrowBook method
+
+
+ // TODO: Add returnBook method
+
+}
+
+class Member {
+
+ // TODO: Add properties as Private
+
+
+ public function __construct($name) {
+
+      // TODO: Initialize properties
+
+     }
+
+
+ // TODO: Add getName method
+
+ 
+ // TODO: Add borrowBook method
+
+ 
+
+ // TODO: Add returnBook method
+
+ 
+}
---
+++ string.php
@@ -0,0 +1,8 @@
+<?php
+// Define the original string
+$strings = ["Hello", "World", "PHP", "Programming"];
+
+print_r($strings)  . "\n"; 
+
+// Print the reversed string
+print_r(strrev($strings)) ; 
